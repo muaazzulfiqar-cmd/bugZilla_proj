bacaf283233593f8b012255bb4817b6f376bdebe
muaazzulfiqar-cmd <muaaz.zulfiqar@stackpinnacle.com>
2026-02-25 19:41:31 +0400

    mail delivery

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -6,7 +6,7 @@ class Users::SessionsController < Devise::SessionsController
 
   def send_login_notification
     if current_user
-      UserMailer.login_notification(current_user, request.remote_ip).deliver_now
+      UserMailer.login_notification(current_user, request.remote_ip).deliver_later
     end
   end
 end
